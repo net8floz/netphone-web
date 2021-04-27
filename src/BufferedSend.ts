18b4c452fe8a3cb438acad0608a6d75c30ed9153
net8floz <net8floz@gmail.com>
2021-04-26 18:15:26 -0700

    some fixes

--- src/BufferedSend.ts
+++ src/BufferedSend.ts
@@ -1,5 +1,13 @@
 type Fn = (...args: any[]) => Promise<any>;
 
+/**
+ * With this you can spam a request and it will wait for a bit before sending it
+ * only sending the last request made. If a request is currently in flight
+ * it will buffer incoming requests until response has been given.
+ *
+ * In this way only the latest data is able to get through. Good for
+ * frequently changes data that needs to be sent
+ */
 export class BufferedSend {
   private sendId = 0;
   private timeoutLength = 500;
@@ -25,5 +33,10 @@ export class BufferedSend {
       console.error(err);
     }
     this.isSending = false;
+    while (this.toSend.length > 0) {
+      const current = this.toSend[0];
+      this.toSend.splice(0, 1);
+      await this.send(current);
+    }
   }
 }
