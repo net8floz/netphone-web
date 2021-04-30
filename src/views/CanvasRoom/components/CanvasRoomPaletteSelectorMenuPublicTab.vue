05e7481478a4b0c31177198531417a3d29e0902d
spacebake <woppetz@gmail.com>
2021-04-30 03:20:17 +0100

    significant style changes to the palette selector menu also added a delete button which only displays on palettes belonging to the current user. ( both of these changes are currently only for the "public" tab).

--- src/views/CanvasRoom/components/CanvasRoomPaletteSelectorMenuPublicTab.vue
+++ src/views/CanvasRoom/components/CanvasRoomPaletteSelectorMenuPublicTab.vue
@@ -12,28 +12,36 @@
     </div>
     <v-list v-else>
       <v-list-item
-        class="pt-2 pb-2"
+        class="pt-2 pb-2 list-block"
         v-for="palette in publicPalettes"
         :key="palette.id"
+
       >
-        <div class="d-flex">
-          <div style="width: 250px" class="d-flex">
+        <div style="width:100%;">
+          <div class="d-flex" style="margin-bottom:-16px;">
             <v-checkbox
               v-model="currentPaletteIds"
               :value="palette.id"
-              class="mt-0 pt-0"
+              class="mt-0 pt-0 mb--5"
             />
+            <v-avatar size="24" class="disc-avatar">
+              <img :src="palette.author.profilePictureUrl" />
+            </v-avatar>
             <div>
               {{ palette.name }}
             </div>
-            <v-avatar size="24" class="ml-1">
-              <img :src="palette.author.profilePictureUrl" />
-            </v-avatar>
+            <v-spacer />
+            <v-btn
+              elevation="2"
+              icon
+              small
+              v-if="allowDelete(palette.author.id)"
+            ><v-icon size="17" nudge-left="20">mdi-close-circle</v-icon></v-btn>
           </div>
-          <div class="d-flex flex-wrap">
+          <div class="d-flex flex-start colorBox-wrap">
             <div
               class="colorBox"
-              v-for="color in palette.colors"
+              v-for="color in palette.colors" 
               :key="color.id"
               :style="`background-color: ${color.hex}`"
             />
@@ -48,6 +56,7 @@
 import { schema } from '@/gql';
 import gql from 'graphql-tag';
 import { Component, Prop, Ref, Vue, Watch } from 'vue-property-decorator';
+
 @Component({
   apollo: {
     publicPalettes: {
@@ -88,6 +97,14 @@ export default class CanvasRoomPaletteSelectorMenuPublicTab extends Vue {
   private publicPalettes: schema.ColorPalette[] = [];
 
   private menu = true;
+  
+  private allowDelete(PaletteAuthId: string): boolean {
+    if (!PaletteAuthId) {
+      return false;
+    }
+    return PaletteAuthId === this.$auth.userId;
+  }
+
 
   @Watch('value')
   private onValueChanged(value: string[]) {
@@ -115,7 +132,23 @@ export default class CanvasRoomPaletteSelectorMenuPublicTab extends Vue {
 
 <style lang="scss" scoped>
 .colorBox {
-  width: 25px;
-  height: 25px;
+    width: 20px;
+    height: 20px;
+
 }
+
+.colorBox-wrap{
+  margin-left:3px;
+}
+
+.list-block{
+  border-top: 1px dotted #393939a1;
+  padding-right:5px;
+}
+
+.disc-avatar{
+  margin-right:10px;
+}
+
+
 </style>
