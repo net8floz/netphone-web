5d59358c902c054aec01f38a8ae284a5a31536eb
net8floz <net8floz@gmail.com>
2021-05-01 16:16:22 -0700

    space changes

--- src/components/GameRoom/GameRoomUserList.vue
+++ src/components/GameRoom/GameRoomUserList.vue
@@ -8,6 +8,28 @@
       <v-list-item-content> Username </v-list-item-content>
     </v-list-item>
   </v-list>
+  <!-- 
+    <v-list class="mt-0 pt-0">
+        <v-list-item v-for="socketUser in room.users" :key="socketUser.id">
+          <v-list-item-avatar size="28" class="mr-2" v-if="!socketUser.isGuest">
+            <img :src="socketUser.profilePictureUrl" />
+          </v-list-item-avatar>
+          <v-list-item-avatar
+            v-else
+            rounded
+            color="primary"
+            size="28"
+            class="pr-1 mr-2 justify-center"
+          >
+            <div>G</div>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ socketUser.displayName }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list> -->
 </template>
 
 <script lang="ts">
